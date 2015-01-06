e3f4843c9650f75099af4f4c152f022946c31c2d
rosswhitehouse <ross.dw@live.com>
2015-01-06 08:45:44 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+
+$('.arrow').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.two').offset().top
+        }, 400);
+});
+
+$('.arrowTwo').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.three').offset().top
+        }, 400);
+});
+
+$('.arrowThree').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.demo').offset().top
+        }, 400);
+});
+
+$('.arrowDemo').click(function() {
+  $('html, body').animate({
+    scrollTop: $('.four').offset().top
+  }, 400);
+});
+
+$('.arrowFour').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.five').offset().top
+        }, 400);
+});
+
+$('.arrowFive').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.six').offset().top
+        }, 400);
+});
+
+$('.arrowSix').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.seven').offset().top
+        }, 400);
+});
+
+$('.arrowSeven').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.eight').offset().top
+        }, 400);
+});
+
+$('.arrowEight').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.nine').offset().top
+        }, 400);
+});
+
+$('.arrowTwo').click(function() {
+  
+  $('html, body').animate({
+            scrollTop: $('.three').offset().top
+        }, 400);
+});
+
+$('.optionOne').click(function(){
+  $('html, body').animate({
+  scrollTop: $('.five').offset().top
+  }, 400);
+});
+
+$('.optionTwo').click(function(){
+  $('html, body').animate({
+  scrollTop: $('.six').offset().top
+  }, 400);
+});
+
+$('.optionThree').click(function(){
+  $('html, body').animate({
+  scrollTop: $('.seven').offset().top
+  }, 400);
+});
+
+$('.optionFour').click(function(){
+  $('html, body').animate({
+  scrollTop: $('.eight').offset().top
+  }, 400);
+});
+
+$('.arrowOptions').click(function(){
+  $('html, body').animate({
+    scrollTop: $('.four').offset().top
+  }, 400);
+});
+
+console.log('working');
